dcb7783bea6784c255bdc953107769cbde700c95
Dylan Stockinger <pro.stockinger@gmail.com>
2023-01-23 12:09:14 +0100

    new cv

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,72 +1,51 @@
 
 
-$(function () {
-    $(".menu-link").click(function () {
-     $(".menu-link").removeClass("is-active");
-     $(this).addClass("is-active");
-    });
-   });
+// $(function () {
+//     $(".menu-link").click(function () {
+//      $(".menu-link").removeClass("is-active");
+//      $(this).addClass("is-active");
+//     });
+//    });
    
-   $(function () {
-    $(".main-header-link").click(function () {
-     $(".main-header-link").removeClass("is-active");
-     $(this).addClass("is-active");
-    });
-   });
+//    $(function () {
+//     $(".main-header-link").click(function () {
+//      $(".main-header-link").removeClass("is-active");
+//      $(this).addClass("is-active");
+//     });
+//    });
    
-   const dropdowns = document.querySelectorAll(".dropdown");
-   dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener("click", (e) => {
-     e.stopPropagation();
-     dropdowns.forEach((c) => c.classList.remove("is-active"));
-     dropdown.classList.add("is-active");
-    });
-   });
+//    const dropdowns = document.querySelectorAll(".dropdown");
+//    dropdowns.forEach((dropdown) => {
+//     dropdown.addEventListener("click", (e) => {
+//      e.stopPropagation();
+//      dropdowns.forEach((c) => c.classList.remove("is-active"));
+//      dropdown.classList.add("is-active");
+//     });
+//    });
    
-   $(".search-bar input")
-    .focus(function () {
-     $(".header").addClass("wide");
-    })
-    .blur(function () {
-     $(".header").removeClass("wide");
-    });
-   
-   $(document).click(function (e) {
-    var container = $(".status-button");
-    var dd = $(".dropdown");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-     dd.removeClass("is-active");
-    }
-   });
-   
-  //  $(function () {
-  //   $(".dropdown").on("click", function (e) {
-  //    $(".content-wrapper").addClass("overlay");
-  //    e.stopPropagation();
+  //  $(".search-bar input")
+  //   .focus(function () {
+  //    $(".header").addClass("wide");
+  //   })
+  //   .blur(function () {
+  //    $(".header").removeClass("wide");
   //   });
-  //   $(document).on("click", function (e) {
-  //    if ($(e.target).is(".dropdown") === false) {
-  //     $(".content-wrapper").removeClass("overlay");
-  //    }
-  //   });
+   
+  //  $(document).click(function (e) {
+  //   var container = $(".status-button");
+  //   var dd = $(".dropdown");
+  //   if (!container.is(e.target) && container.has(e.target).length === 0) {
+  //    dd.removeClass("is-active");
+  //   }
   //  });
    
-  //  $(function () {
-  //   $(".status-button:not(.open)").on("click", function (e) {
-  //    $(".overlay-app").addClass("is-active");
-  //   });
-  //   $(".pop-up .close").click(function () {
-  //    $(".overlay-app").removeClass("is-active");
-  //   });
+  //  $(".status-button:not(.open)").click(function () {
+  //   $(".pop-up").addClass("visible");
   //  });
    
-   $(".status-button:not(.open)").click(function () {
-    $(".pop-up").addClass("visible");
-   });
-   
-   $(".pop-up .close").click(function () {
-    $(".pop-up").removeClass("visible");
-   });
+  //  $(".pop-up .close").click(function () {
+  //   $(".pop-up").removeClass("visible");
+  //  });
    
    const toggleButton = document.querySelector('.dark-light');
    
